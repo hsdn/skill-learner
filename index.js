b3c2a2157d8fb55756ab4c37f95416e4067fe703
Dmitry Shin <dmitry.s@hsdn.org>
2021-04-28 10:29:01 +0400

    Update

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 module.exports = function learnSkill(mod) {
-	const { 'tera-game-state': game } = mod.require;
-	game.initialize(['inventory', 'contract']);
-	const { me, inventory } = game;
+	mod.game.initialize(["inventory", "contract"]);
 
-	const reqSkillList = () => mod.send('C_SKILL_LEARN_LIST', 2);
+	const reqSkillList = () => mod.send("C_SKILL_LEARN_LIST", 2);
 
 	const reqSkillListWhenPossible = () => {
-		if (me.inCombat) {
-			me.once('leave_combat', () => {
-				if (me.mounted) me.once('dismount', reqSkillList);
-				else reqSkillList();
+		if (mod.game.me.inCombat) {
+			mod.game.me.once("leave_combat", () => {
+				if (mod.game.me.mounted)
+					mod.game.me.once("dismount", reqSkillList);
+				else
+					reqSkillList();
 			});
-		} else if (me.mounted) {
-			me.once('dismount', () => {
-				if (me.inCombat) me.once('leave_combat', reqSkillList);
-				else reqSkillList();
+		} else if (mod.game.me.mounted) {
+			mod.game.me.once("dismount", () => {
+				if (mod.game.me.inCombat)
+					mod.game.me.once("leave_combat", reqSkillList);
+				else
+					reqSkillList();
 			});
-		} else reqSkillList();
+		} else
+			reqSkillList();
 	};
 
-	me.on('change_level', level => {
-		if (level <= 65) reqSkillListWhenPossible();
+	mod.game.me.on("change_level", level => {
+		if (level <= 65)
+			reqSkillListWhenPossible();
 	});
 
-	mod.hook('S_SKILL_LEARN_LIST', 2, e => {
+	mod.hook("S_SKILL_LEARN_LIST", 2, e => {
 		for (const skill of e.skills) {
-			if (!skill.active || !skill.available || skill.requiredMoney >= inventory.money) continue;
-			mod.send('C_SKILL_LEARN_REQUEST', 2, { id: skill.id, active: true });
+			if (!skill.active || !skill.available || skill.requiredMoney >= mod.game.inventory.money)
+				continue;
+
+			mod.send("C_SKILL_LEARN_REQUEST", 2, { "id": skill.id, "active": true });
 		}
 	});
 };
